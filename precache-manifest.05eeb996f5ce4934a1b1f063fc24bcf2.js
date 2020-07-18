e41ff0482b94b0f0b2c8567f3420dd96e85dcf81
Weike Shi <shiw14@mcmaster.ca>
2020-07-18 11:29:45 -0400

    Updates

---
+++ precache-manifest.05eeb996f5ce4934a1b1f063fc24bcf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b3811f7b42f5e49dfeee16c302dbd7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1ae658bedb7466f96818",
+    "url": "/monsters-rolodex/static/css/main.62d87302.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae658bedb7466f96818",
+    "url": "/monsters-rolodex/static/js/main.77df9897.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
